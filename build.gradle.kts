8ce8fe31ed23d9fa9874602189e1069e0ade2417
Josef Andersson <josef.andersson@svt.se>
2022-05-25 14:51:21 +0200

    Maintenance update - publishing, workflow (#5)
    * docs: cleanup README. update CONTRIBUTING
    
    Signed-off-by: Josef Andersson <josef.andersson@svt.se>
    
    * chore: update dep and gradle
    
    Signed-off-by: Josef Andersson <josef.andersson@svt.se>
    
    * chore: updated publishing plugin and properties
    
    Signed-off-by: Josef Andersson <josef.andersson@svt.se>
    
    * chore: correct readme link and signing prop
    
    Signed-off-by: Josef Andersson <josef.andersson@svt.se>
    
    * chore: convert gradle build file to kotlin dsl
    
    Signed-off-by: Josef Andersson <josef.andersson@svt.se>
    
    * ci: update and improve ci workflow
    
    Signed-off-by: Josef Andersson <josef.andersson@svt.se>
    
    * chore: linted project. renamed build yml
    
    Signed-off-by: Josef Andersson <josef.andersson@svt.se>
    
    * chore: make project reuse compliant
    
    Signed-off-by: Josef Andersson <josef.andersson@svt.se>
    
    * chore: gradlew 7.4.2
    
    Signed-off-by: Josef Andersson <josef.andersson@svt.se>
    
    * chore: prepare changelog
    
    Signed-off-by: Josef Andersson <josef.andersson@svt.se>
    
    * ci: change main to master in workflow
    
    Signed-off-by: Josef Andersson <josef.andersson@svt.se>
    
    * ci: add conf for axion to handle legacy release tags
    
    Signed-off-by: Josef Andersson <josef.andersson@svt.se>
    
    * chore: remove unreliable badge link to opensource license
    
    Signed-off-by: Josef Andersson <josef.andersson@svt.se>
    
    * ci: add property directReleaseToMavenCentral
    
    Signed-off-by: Josef Andersson <josef.andersson@svt.se>
    
    * ci: add maven central legacy url prop
    
    Signed-off-by: Josef Andersson <josef.andersson@svt.se>

---
+++ build.gradle.kts
@@ -0,0 +1,51 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    `java-library`
+    kotlin("jvm") version "1.6.21"
+    id("org.jmailen.kotlinter") version "3.10.0"
+    id("pl.allegro.tech.build.axion-release") version "1.13.7"
+    id("se.svt.oss.gradle-yapp-publisher") version "0.1.18"
+    id("se.ascp.gradle.gradle-versions-filter") version "0.1.16"
+}
+
+scmVersion.tag.prefix = "release"
+scmVersion.tag.versionSeparator = "-"
+
+group = "se.svt.oss"
+project.version = scmVersion.version
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+    }
+}
+
+configurations.all {
+    exclude(module = "junit")
+}
+
+kotlin {
+    jvmToolchain {
+        (this as JavaToolchainSpec).languageVersion.set(JavaLanguageVersion.of(11))
+    }
+}
+
+dependencies {
+    api(kotlin("reflect"))
+    api("org.springframework:spring-context:5.3.20")
+
+    testImplementation("org.assertj:assertj-core:3.22.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testImplementation("org.springframework:spring-test:5.3.20")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+}
+
+tasks.wrapper {
+    distributionType = Wrapper.DistributionType.ALL
+    gradleVersion = "7.4.2"
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "random-port-initializer"
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
